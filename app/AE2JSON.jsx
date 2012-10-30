8b2be1dcec8d2148829b8504f63f7b88d74849ae
Cole Reed <ichabodcole@gmail.com>
2012-10-30 01:42:59 -0700

    Delete Help Window, won't need it for this script

---
+++ app/AE2JSON.jsx
@@ -0,0 +1,8 @@
+// Bouncy! v0.1
+//
+// Copyright (c) 2012 Cole Reed. All rights reserved.
+// email: info AT auralgrey DOT com    
+//
+// This script provides the ability to export AE 3D scene data into JSON
+
+#include "../lib/Logger.jsx";
